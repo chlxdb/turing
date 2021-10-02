58a97115f064305e1e885d0c8c85d70c8f88dcb7
xddno1 <2196815716@qq.com>
2021-10-02 13:53:52 +0800

    app改成class

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import { Link } from 'react-router-dom'
-import ViewRouter from './router/index'
+import { Link } from "react-router-dom";
+import ViewRouter from "./router/index";
 // 封装好的路由
-import './App.css'
+import "./App.css";
 
-import { Layout, Menu } from 'antd'
+import { Layout, Menu } from "antd";
+import React, { Component } from "react";
 // import leader from './pages/Leader'
 // import RouterView from './router/router-view'
 
-const { Header, Footer, Content } = Layout
+const { Header, Footer, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <Header>
-          <Menu
-            style={{ marginTop: '20px' }}
-            className="nar"
-            mode="horizontal"
-            defaultSelectedKeys={['1']}
-          >
-
-            <Link to="/index">
-              <Menu.Item key={1}>首页</Menu.Item>
-            </Link>
-            <Link to="/leader">
-              <Menu.Item key={2}>领导视察</Menu.Item>
-            </Link>
-            <Link to="/scale">
-              <Menu.Item key={3}>团队规模</Menu.Item>
-            </Link>
-            <Link to="/project">
-              <Menu.Item key={4}>团队项目</Menu.Item>
-            </Link>
-            <Link to="/honor">
-              <Menu.Item key={5}>团队荣誉</Menu.Item>
-            </Link>
-            <Link to="/join">
-              <Menu.Item key={6}>加入我们</Menu.Item>
-            </Link>
-          </Menu>
-        </Header>
-        <Content>
-          <ViewRouter></ViewRouter>
-        </Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <Header>
+            <Menu
+              style={{ marginTop: "20px" }}
+              className="nar"
+              mode="horizontal"
+              defaultSelectedKeys={["1"]}
+            >
+              <Link to="/index">
+                <Menu.Item key={1}>首页</Menu.Item>
+              </Link>
+              <Link to="/leader">
+                <Menu.Item key={2}>领导视察</Menu.Item>
+              </Link>
+              <Link to="/scale">
+                <Menu.Item key={3}>团队规模</Menu.Item>
+              </Link>
+              <Link to="/project">
+                <Menu.Item key={4}>团队项目</Menu.Item>
+              </Link>
+              <Link to="/honor">
+                <Menu.Item key={5}>团队荣誉</Menu.Item>
+              </Link>
+              <Link to="/join">
+                <Menu.Item key={6}>加入我们</Menu.Item>
+              </Link>
+            </Menu>
+          </Header>
+          <Content>
+            <ViewRouter></ViewRouter>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </div>
+    );
+  }
 }
-export default App
