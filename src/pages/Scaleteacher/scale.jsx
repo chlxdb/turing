f3dcf6c12b3a592ed0196fb4d2c823f00d11460e
chlxdb <2214631535@qq.com>
2021-10-04 19:23:58 +0800

    my

--- src/pages/Scaleteacher/scale.jsx
+++ src/pages/Scaleteacher/scale.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class scale extends Component {
+export default class scaletea extends Component {
   goRouter = (path) => {
     return () => {
       this.props.history.push({
